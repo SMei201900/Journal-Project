cd06e449d4a6803d161c91b96ea7a31462d4adb2
Sally M <smei201900@gmail.com>
2025-11-05 19:47:09 -0500

    I want to click the button and have it change text

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let plusSymbol = document.getElementById("plusSymbol");
+
 plusSymbol.addEventListener("click", () => {
+    event.preventDefault(); /*prevent the page from reloading*/
+    plusSymbol.textContent = "Create a new post"; /*changes the text of the button*/
     console.log("Clicked")
 })
