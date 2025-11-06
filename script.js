5a0621587c8cb12af5bf153b397d262f16ade8cb
Sally M <smei201900@gmail.com>
2025-11-05 19:47:39 -0500

    tried out a sliding animation to the button

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ let plusSymbol = document.getElementById("plusSymbol");
 plusSymbol.addEventListener("click", () => {
     event.preventDefault(); /*prevent the page from reloading*/
     plusSymbol.textContent = "Create a new post"; /*changes the text of the button*/
+    plusSymbol.classList.add("slide-in");
+        /*add a "class" called "slide-in" 
+        --intent: adds a "sliding in from the left" animation*/
     console.log("Clicked")
 })
