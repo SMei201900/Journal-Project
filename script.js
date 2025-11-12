d81d4a7ec1a0e31fc91dda9bfaebbdfbc0c8fb78
Sally M <smei201900@gmail.com>
2025-11-12 02:22:58 -0500

    this does move the plusBtn somewhere to the middle of the screen, not exact but proves the code is working

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 let plusSymbol = document.getElementById("plusSymbol");
 
 plusSymbol.addEventListener("click", () => {
-    event.preventDefault(); /*prevent the page from reloading*/
-    plusSymbol.textContent = "Create a new post"; /*changes the text of the button*/
-    console.log("Clicked")
-})
+  event.preventDefault(); /*prevent the page from reloading*/
+  plusSymbol.textContent = "Create a new post"; /*changes the text of the button*/
+  console.log("Clicked");
+  plusSymbol.classList.add("moveBtnToCenter");
 
-
-
+});
 
 /*
 .remove() is how to remove 
 */
-
