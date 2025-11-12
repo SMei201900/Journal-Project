aeb64fc48a8d5ebcbf646f79528b124ee48cbddf
Sally M <smei201900@gmail.com>
2025-11-12 02:30:04 -0500

    moving the button to the middle

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ plusSymbol.addEventListener("click", () => {
   event.preventDefault(); /*prevent the page from reloading*/
   plusSymbol.textContent = "Create a new post"; /*changes the text of the button*/
   console.log("Clicked");
-  plusSymbol.classList.add("moveBtnToCenter");
 
 });
 
 /*
 .remove() is how to remove 
+plusSymbol.classList.add("")    how to add a classList 
 */
