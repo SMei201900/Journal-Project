76fba580ef03327ccb07f1cdad8742cc8f1c413f
Sally M <smei201900@gmail.com>
2025-11-12 03:25:07 -0500

    okay i figured how to create and style a textarea in html&css, now I want to move all that into JS

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+const { createElement } = require("react");
+
 let plusSymbol = document.getElementById("plusSymbol");
 
 plusSymbol.addEventListener("click", () => {
   event.preventDefault(); /*prevent the page from reloading*/
   plusSymbol.textContent = "Create a new post"; /*changes the text of the button*/
   console.log("Clicked");
+  let journalBox = createElement("textarea")
 
 });
 
