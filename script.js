9994897bf6850b7080fcc9775ba11816056d2258
Sally M <smei201900@gmail.com>
2025-11-12 20:34:46 -0500

    Implemented clicking the button means textarea shows up and therefore we can type

--- script.js
+++ script.js
@@ -4,17 +4,18 @@ let plusSymbol = document.getElementById("plusSymbol");
 
 plusSymbol.addEventListener("click", () => {
   event.preventDefault(); /*prevent the page from reloading*/
-  plusSymbol.textContent = "Create a new post"; /*changes the text of the button*/
+  plusSymbol.textContent = "something was done"; /*changes the text of the button*/
   console.log("Clicked")
-/*I want the click plus button to untransparent the textarea*/
+  let textfield = document.querySelector("textarea");
+  textfield.id = "journalBox"
+  textfield.placeholder = "Today I ... "
+  textfield.classList.add = "placeholder"
 });
 
 
 /*NEXT STEPS: 
-change +symbol to NEW POST as original text 
-And then click will untransparent the textarea 
-I need to create a post button*/
-
+I need to create a post button
+the resize(?) of the textarea, needs to be darker /bigger or something */
 
 
 /*
@@ -22,19 +23,6 @@ I need to create a post button*/
 plusSymbol.classList.add("")    how to add a classList 
 */
 
-
-
-//NOTES 
-// #myTextarea {
-//   background-color: rgba(255, 255, 255, 0); /* Initial transparent background */
-//   border: 1px solid rgba(0, 0, 0, 0.2); /* Subtle, semi-transparent border */
-//   transition: background-color 0.3s ease, border-color 0.3s ease; /* Smooth transition */
-// }
-
-// #myTextarea:focus {
-//   background-color: rgba(255, 255, 255, 1); /* Opaque background on focus */
-//   border-color: rgba(0, 0, 0, 0.8); /* More prominent border on focus */
-// }
 
 /*custom cursor? 
 https://img.freepik.com/premium-vector/carddiamond-icon-isolated-white-background_268104-11359.jpg 
